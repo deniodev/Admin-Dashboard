259802b7abe3c7351837565a278fe62acb321466
Denio Nhanale <dnhanale@gmail.com>
2024-01-23 15:21:42 +0200

    Fecth Item with Pagination

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -7,7 +7,8 @@ import { fetchUsers } from "../../lib/data";
 
 const Userspage = async ({searchParams}) => {
   const q = searchParams?.q || "";
-  const users = await fetchUsers(q);
+  const page = searchParams?.page || 1;
+  const {count,users} = await fetchUsers(q,page);
 
   return (
     <div className={styles.container}>
@@ -59,7 +60,7 @@ const Userspage = async ({searchParams}) => {
           ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count}/>
     </div>
   )
 }
