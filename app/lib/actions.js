3f93120c983e5597f837b83a46a603187656df30
Denio Nhanale <dnhanale@gmail.com>
2024-01-24 11:10:59 +0200

    Use server actions to update users and products

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -76,7 +76,7 @@ export const addUser = async (formData) => {
       
         revalidatePath("/dashboard/products");
       };
-      
+
       export const deleteProduct = async (formData) => {
         const { id } = Object.fromEntries(formData);
       
@@ -89,4 +89,67 @@ export const addUser = async (formData) => {
         }
       
         revalidatePath("/dashboard/products");
+      };
+
+      export const updateUser = async (formData) => {
+        const { id, username, email, password, phone, address, isAdmin, isActive } =
+          Object.fromEntries(formData);
+      
+        try {
+          connectToDB();
+      
+          const updateFields = {
+            username,
+            email,
+            password,
+            phone,
+            address,
+            isAdmin,
+            isActive,
+          };
+      
+          Object.keys(updateFields).forEach(
+            (key) =>
+              (updateFields[key] === "" || undefined) && delete updateFields[key]
+          );
+      
+          await User.findByIdAndUpdate(id, updateFields);
+        } catch (err) {
+          console.log(err);
+          throw new Error("Failed to update user!");
+        }
+      
+        revalidatePath("/dashboard/users");
+        redirect("/dashboard/users");
+      };
+
+      export const updateProduct = async (formData) => {
+        const { id, title, desc, price, stock, color, size } =
+          Object.fromEntries(formData);
+      
+        try {
+          connectToDB();
+      
+          const updateFields = {
+            title,
+            desc,
+            price,
+            stock,
+            color,
+            size,
+          };
+      
+          Object.keys(updateFields).forEach(
+            (key) =>
+              (updateFields[key] === "" || undefined) && delete updateFields[key]
+          );
+      
+          await Product.findByIdAndUpdate(id, updateFields);
+        } catch (err) {
+          console.log(err);
+          throw new Error("Failed to update product!");
+        }
+      
+        revalidatePath("/dashboard/products");
+        redirect("/dashboard/products");
       };
